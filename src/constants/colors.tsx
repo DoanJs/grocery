9520595931eab931860970dab421b11e7e1a84cd
Mac <mac@Ny.lan>
2025-07-29 09:12:40 +0700

    add fonts poppins

---
+++ src/constants/colors.tsx
@@ -0,0 +1,12 @@
+export const colors = {
+    background: '#FFFFFF',
+    background1: '#F4F5F9',
+    background2: '#F5F5F5',
+    text: '#868889',
+    text2: '#dddddd',
+    link: '#1A0DAB',
+    border: '#EBEBEB',
+    primary: '#6CC51D',
+    primaryDark: '#AEDC81',
+    primaryLight: '#EBFFD7',
+}
